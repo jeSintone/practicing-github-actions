34cb01879755d7c8b2028dd696d3e9f49aada818
Jesse Sintonen <sintonej.esse@gmail.com>
2026-04-28 14:24:25 +0300

    Update Workout.js

--- testing-with-workout-project-github-classroom/Workout.js
+++ testing-with-workout-project-github-classroom/Workout.js
@@ -6,7 +6,7 @@ const createWorkout = username => {
     if (typeof username !== 'string' || username.length < 4) {
         throw new Error("the parameter username is not valid");
     }
-    return db('workout').insert({ username });
+    return db('workouts').insert({ username });
 };
 
 const addExercise = (workoutId, exerciseName) => {
